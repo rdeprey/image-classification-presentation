7954e90967cd43ee3b887e31c559aa09534ff362
Rebecca Deprey <rdeprey@gmail.com>
2019-12-01 11:43:05 -0500

    Updating repo for Github Pages hosting

---
+++ build/precache-manifest.6f337ec78d320f17e898a7376ee4e35d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d83017e3201f0630251",
+    "url": "/static/js/main.5d83017e.chunk.js"
+  },
+  {
+    "revision": "a4e3d52d644ff9ed9222",
+    "url": "/static/js/1.a4e3d52d.chunk.js"
+  },
+  {
+    "revision": "5d83017e3201f0630251",
+    "url": "/static/css/main.51f56265.chunk.css"
+  },
+  {
+    "revision": "a4e3d52d644ff9ed9222",
+    "url": "/static/css/1.f6dd08ca.chunk.css"
+  },
+  {
+    "revision": "bd8c65641990910f90096fcb795cf098",
+    "url": "/index.html"
+  }
+];

53ecfa91a1c2733d7fea48e0959e56c5fd6b2d5a
Rebecca Deprey <rdeprey@gmail.com>
2020-03-22 14:59:06 -0400

    Updates

---
+++ precache-manifest.6624d449b0012795ba330959bc0bbf87.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f5a69bf40729467de04e",
+    "url": "/image-classification-presentation/static/js/runtime~main.f5a69bf4.js"
+  },
+  {
+    "revision": "5d83017e3201f0630251",
+    "url": "/image-classification-presentation/static/js/main.5d83017e.chunk.js"
+  },
+  {
+    "revision": "a4e3d52d644ff9ed9222",
+    "url": "/image-classification-presentation/static/js/1.a4e3d52d.chunk.js"
+  },
+  {
+    "revision": "5d83017e3201f0630251",
+    "url": "/image-classification-presentation/static/css/main.51f56265.chunk.css"
+  },
+  {
+    "revision": "a4e3d52d644ff9ed9222",
+    "url": "/image-classification-presentation/static/css/1.f6dd08ca.chunk.css"
+  },
+  {
+    "revision": "9a0f70ba39a55aca497a9aae01be36ff",
+    "url": "/image-classification-presentation/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7a5e07a9bffffd2933f6265ca2ecf8ab.js"
+  "/image-classification-presentation/precache-manifest.6624d449b0012795ba330959bc0bbf87.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/image-classification-presentation/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
